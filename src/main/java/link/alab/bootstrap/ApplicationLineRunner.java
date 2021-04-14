3582215b631d95ff84cd984d05ccf59f2917dae3
arun k <kumararun@virtusa.com>
2021-04-14 10:40:39 +0530

    fix annotation

--- src/main/java/link/alab/bootstrap/ApplicationLineRunner.java
+++ src/main/java/link/alab/bootstrap/ApplicationLineRunner.java
@@ -5,11 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import link.alab.model.Person;
 import link.alab.repository.PersonRespository;
 import reactor.core.publisher.Flux;
-
+@Component
 public class ApplicationLineRunner implements CommandLineRunner {
 	@Autowired
 	PersonRespository personRespository;
