03bddfcbad653cca2a47ca7f6583fdbb67092989
arun k <kumararun@virtusa.com>
2021-04-14 10:33:33 +0530

    flux subscription

---
+++ src/main/java/link/alab/bootstrap/ApplicationLineRunner.java
@@ -0,0 +1,29 @@
+package link.alab.bootstrap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import link.alab.model.Person;
+import link.alab.repository.PersonRespository;
+import reactor.core.publisher.Flux;
+
+public class ApplicationLineRunner implements CommandLineRunner {
+	@Autowired
+	PersonRespository personRespository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<Person> personList=new ArrayList<>();
+		Person person=new Person();
+		person.setAge(2);
+		person.setName("Shravya");
+		personList.add(person);
+		personRespository.insert(Flux.fromIterable(personList)).log()
+		  .subscribe();
+		
+	}
+
+}
--- src/main/java/link/alab/repository/PersonRespository.java
+++ src/main/java/link/alab/repository/PersonRespository.java
@@ -1,10 +1,11 @@
 package link.alab.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
 import link.alab.model.Person;
 import reactor.core.publisher.Flux;
-
+@Repository
 public interface PersonRespository extends ReactiveMongoRepository<Person, String> {
     Flux<Person> findByName(String name);
 }
