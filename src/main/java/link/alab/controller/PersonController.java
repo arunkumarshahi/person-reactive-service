a6c598a6569b0144b3ec3d887907156a1a0af625
arunkumarshahi@gmail.com <arunkumarshahi@gmail.com>
2021-04-21 01:08:47 +0530

    fix

--- src/main/java/link/alab/controller/PersonController.java
+++ src/main/java/link/alab/controller/PersonController.java
@@ -10,6 +10,7 @@ import org.springframework.web.server.ServerWebInputException;
 import link.alab.model.Person;
 import link.alab.repository.PersonRespository;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.method;
@@ -18,11 +19,18 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.n
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
-import org.springframework.context.annotation.Configuration;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.web.reactive.function.BodyExtractors.toMono;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 @Configuration
 @Slf4j
 public class PersonController {
+	@Autowired
+	PersonRespository personRespository;
 	@Bean
 	RouterFunction<?> routes(PersonRespository personRespository) {
 		return nest(path("/person"),
@@ -33,12 +41,35 @@ public class PersonController {
 								.andRoute(method(HttpMethod.POST), request -> {
 									(request.bodyToMono(Person.class))
 									.switchIfEmpty(Mono.error(new ServerWebInputException("Request body cannot be empty.")))
-									.doOnNext(personRespository::insert)
+									.doOnNext(personRespository::save)
 											.log("inserting in controller :: ")
 											
 											.subscribe(x -> log.info("message :: {}", x),
 													ex -> log.error("error :: {}", ex.getMessage()));
 									return ok().build();
-								}));
+								})
+								.andRoute(POST("/update"), 
+									      req -> req.body(toMono(Person.class))
+									        .doOnNext(personRespository::save)
+									        .log()
+									        .then(ok().build()))	
+				);
 	}
+	
+	 @Bean
+	    RouterFunction<ServerResponse> updateEmployeeRoute() {
+		 List<Person> personList=new ArrayList<>();
+			Person person=new Person();
+			person.setAge(2);
+			person.setName("Arun k");
+			personList.add(person);
+			personRespository.insert(Flux.fromIterable(personList)).log()
+			  .subscribe();
+			
+	      return route(POST("/employees/update"), 
+	        req -> req.body(toMono(Person.class))
+	                  .doOnNext(personRespository::save)
+	                  .log()
+	                  .then(ok().build()));
+	    }
 }
